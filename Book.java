e0a7ff2ad652f7d9490db800adedb0564e5042b0
fuzzyliam780 <47114055+fuzzyliam780@users.noreply.github.com>
2019-02-04 12:35:12 -0500

    2.88 & 2.89 - added String refNumber getter setter, adjusted constructer and printDetails

--- Book.java
+++ Book.java
@@ -11,17 +11,19 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private String refNumber = "";
     private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int bookPages)
+    public Book(String bookAuthor, String bookTitle, String bookRefNumber, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = bookRefNumber;
     }
     
     /**
@@ -52,6 +54,23 @@ class Book
     }
     
     /**
+     * Provide access to string containing the book's reference number
+     * @return refNumber
+     */
+    public String getRefNumber()
+    {
+        return refNumber;
+    }
+    
+    /**
+     * Set string containing the book's reference number to be a provided reference number
+     */
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+    
+    /**
      * Print the author of the book to the terminal
      */
     public void printAuthor()
@@ -72,6 +91,11 @@ class Book
      */
     public void printDetails()
     {
-        System.out.println("Title: "+title+"\nAuthor: "+author+"\nPages: "+pages);
+        System.out.println("Title: "+title);
+        System.out.println("\nAuthor: "+author);
+        if (refNumber != ""){
+        System.out.println("\nReference Number: "+refNumber);
+        }
+        System.out.println("\nPages: "+pages);
     }
 }
