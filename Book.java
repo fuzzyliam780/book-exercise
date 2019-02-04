9b68a8864d5e26637748e1202c9fa2f4334b8287
fuzzyliam780 <47114055+fuzzyliam780@users.noreply.github.com>
2019-02-04 12:13:23 -0500

    2.85 - added int pages to fields, book contructor and getPages

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
     
     /**
@@ -38,6 +40,15 @@ class Book
     public String getTitle()
     {
         return title;
+    }
+    
+    /**
+     * Provide access to int containing number of pages in the book
+     * @return pages
+     */
+    public int getPages()
+    {
+        return pages;
     }
     
     /**
